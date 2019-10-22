fcc162aba240425270ea4e158b28a84e17d91e25
saswatijena <saswatisucharitajena@gmail.com>
2019-10-22 11:17:05 -0700

    Added remove to singly linked list

---
+++ src/DataStructures/LinkedList/SinglyLinkedList.js
@@ -0,0 +1,150 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  /** Add node at the end of the list */
+  push(value) {
+    if (value === null || value === undefined)
+      throw Error("passed in value cannot be null or undefined");
+    let newNode = new Node(value);
+    if (!this.head) {
+      // if head is null create the node and point both head and tail to the node
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // else create the node point the tail node's next and tail to the new node
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    // increment length
+    this.length++;
+    return this;
+  }
+  /** Removes node at the end of the list */
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let tempTail = this.head;
+    while (current.next) {
+      tempTail = current;
+      current = current.next;
+    }
+    tempTail.next = null;
+    this.tail = tempTail;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  /** Removes node from the head */
+  shift() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    if (current.next) {
+      this.head = current.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+    this.length--;
+
+    return current;
+  }
+  /** Adds node to the head */
+  unshift(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  /** Takes an index and returns value of that position */
+  get(index) {
+    if (index < 0 || index >= this.length)
+      throw new Error("index is out of bound");
+    let current = this.head;
+    let counter = 0;
+    while (counter < index) {
+      current = current.next;
+      counter++;
+    }
+    // if (!current && index >= counter) throw new Error('index is out of bound');
+    return current;
+  }
+  /** Takes index and value and sets the value at the index */
+  set(index, value) {
+    if (index < 0 || index >= this.length)
+      throw new Error("index is out of bound");
+    // let current = this.head;
+    // let counter = 0;
+
+    // while (counter < index) {
+    //     current = current.next;
+    //     counter++;
+    // }
+    // current.value = value;
+    // return current;
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.value = value;
+      return true;
+    }
+    return false;
+  }
+  /** Inserts node at the specified index */
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+
+    if (index === 0) {
+      return !!this.unshift(value);
+    }
+    if (index === this.length) {
+      return !!this.push(value);
+    }
+
+    let newNode = new Node(value);
+    let perviousNode = this.get(index - 1);
+
+    newNode.next = perviousNode.next;
+    perviousNode.next = newNode;
+    this.length++;
+
+    return true;
+  }
+  /** Removes node from the specific index */
+  remove(index) {
+    if (index < 0 || index >= this.length)
+      throw new Error("out of bound exception");
+
+    if (index === 0) {
+      return this.shift();
+    } else if (index === this.length - 1) {
+      return this.pop();
+    } else {
+      // get the item at the previous index and point next of the next;
+      let previousNode = this.get(index - 1);
+      const removedNode = previousNode.next;
+      previousNode.next = removedNode.next;
+      this.length--;
+      return removedNode;
+    }
+  }
+}
+
+module.exports = SinglyLinkedList;
---
+++ src/DataStructures/LinkedList/SinglyLinkedList.test.js
@@ -0,0 +1,266 @@
+let expect = require("chai").expect;
+let List = require("./SinglyLinkedList");
+
+describe("Push", () => {
+  it("should push items", () => {
+    let list = new List();
+    expect(list.length).to.equal(0);
+    list.push(10);
+    list.push(20);
+
+    let current = list.head;
+    expect(current.value).to.equal(10);
+    expect(list.length).to.equal(2);
+
+    current = current.next;
+    expect(current.value).to.equal(20);
+    expect(list.tail.value).to.equal(20);
+    expect(list.length).to.equal(2);
+
+    list.push(30);
+    current = current.next;
+    expect(current.value).to.equal(30);
+    expect(list.tail.value).to.equal(30);
+    expect(list.length).to.equal(3);
+  });
+});
+describe("Pop", () => {
+  it("should pop items when there are more than one node", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    expect(list.length).to.equal(3);
+    list.pop();
+
+    expect(list.length).to.equal(2);
+    let current = list.head;
+    expect(current.value).to.equal(10);
+    current = current.next;
+    expect(current.value).to.equal(20);
+    expect(current.next).to.be.null;
+  });
+  it("should pop items when there is only one node", () => {
+    let list = new List();
+    list.push(10);
+    list.pop();
+
+    let current = list.head;
+    expect(current).to.be.null;
+    expect(list.length).to.equal(0);
+  });
+  it("should return undefined for pop items when there is no node", () => {
+    let list = new List();
+    let output = list.pop();
+    expect(output).to.be.undefined;
+    expect(list.length).to.equal(0);
+  });
+});
+describe("Shift", () => {
+  it("should shift items if there are more than one node", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+    expect(list.length).to.equal(3);
+    let shiftedNode = list.shift();
+    expect(shiftedNode.value).to.equal(10);
+    expect(list.length).to.equal(2);
+  });
+  it("should shift items if there is only one node", () => {
+    let list = new List();
+    list.push(10);
+
+    let shiftedNode = list.shift();
+    expect(shiftedNode.value).to.equal(10);
+    expect(list.head).to.be.null;
+    expect(list.tail).to.be.null;
+    expect(list.length).to.equal(0);
+  });
+  it("should return undefined for shift items when there is no node", () => {
+    let list = new List();
+    let output = list.shift();
+    expect(output).to.be.undefined;
+    expect(list.length).to.equal(0);
+  });
+});
+describe("Unshift", () => {
+  it("should unshift items when there are no items in the list", () => {
+    let list = new List();
+    list.unshift(10);
+
+    expect(list.head.value).to.equal(10);
+    expect(list.tail.value).to.equal(10);
+    expect(list.length).to.equal(1);
+  });
+  it("should unshift items when there are already items in the list", () => {
+    let list = new List();
+    list.unshift(10);
+    list.unshift(20);
+
+    expect(list.head.value).to.equal(20);
+    expect(list.tail.value).to.equal(10);
+    expect(list.length).to.equal(2);
+
+    list.unshift(30);
+
+    expect(list.head.value).to.equal(30);
+    expect(list.tail.value).to.equal(10);
+    expect(list.length).to.equal(3);
+  });
+});
+describe("Get", () => {
+  it("should return the correct value when the index is greater than 0", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+    list.push(40);
+
+    let node = list.get(1);
+    expect(node.value).to.equal(20);
+    node = list.get(0);
+    expect(node.value).to.equal(10);
+    node = list.get(3);
+    expect(node.value).to.equal(40);
+  });
+  it("should throw error when index is out of range", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    expect(() => list.get(10)).to.throw("index is out of bound");
+    expect(() => list.get(3)).to.throw("index is out of bound");
+  });
+  it("should throw error when index is less than 0", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    expect(() => list.get(-10)).to.throw("index is out of bound");
+    expect(() => list.get(-3)).to.throw("index is out of bound");
+  });
+});
+describe("Set", () => {
+  it("should set the correct value when the index is greater than 0", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+    list.push(40);
+
+    let node = list.set(1, 200);
+    expect(list.get(1).value).to.equal(200);
+    node = list.set(0, 100);
+    expect(list.get(0).value).to.equal(100);
+    node = list.set(3, 400);
+    expect(list.get(3).value).to.equal(400);
+  });
+  it("should throw error when index is out of range", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    expect(() => list.set(10, 200)).to.throw("index is out of bound");
+    expect(() => list.set(3, 200)).to.throw("index is out of bound");
+  });
+  it("should throw error when index is less than 0", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    expect(() => list.set(-10, 200)).to.throw("index is out of bound");
+    expect(() => list.set(-3, 200)).to.throw("index is out of bound");
+  });
+});
+describe("Insert", () => {
+  it("should insert when the index is 0", () => {
+    let list = new List();
+    list.insert(0, 100);
+    expect(list.get(0).value).to.equal(100);
+    expect(list.length).to.equal(1);
+  });
+  it("should insert when the index is equal to the length", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    list.insert(3, 2000);
+    expect(list.get(3).value).to.equal(2000);
+    expect(list.length).to.equal(4);
+  });
+  it("should insert when the index somewhere in between", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    list.insert(1, 2000);
+    expect(list.get(1).value).to.equal(2000);
+    expect(list.get(2).value).to.equal(20);
+    expect(list.length).to.equal(4);
+  });
+  it("should increment the length by 1 when inserted", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    list.insert(1, 2000);
+    expect(list.length).to.equal(4);
+  });
+});
+describe("Remove", () => {
+  it("should remove from the head if given index is at the begining", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    let removedNode = list.remove(0);
+    expect(removedNode.value).to.equal(10);
+    expect(list.head.value).to.equal(20);
+    expect(list.tail.value).to.equal(30);
+    expect(list.length).to.equal(2);
+  });
+  it("should remove from the tail if given index is at the end", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    let removedNode = list.remove(2);
+    expect(removedNode.value).to.equal(30);
+    expect(list.head.value).to.equal(10);
+    expect(list.tail.value).to.equal(20);
+    expect(list.length).to.equal(2);
+  });
+  it("should remove from the head if given index is somewhere in between", () => {
+    let list = new List();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    let removedNode = list.remove(1);
+    expect(removedNode.value).to.equal(20);
+    expect(list.head.value).to.equal(10);
+    expect(list.tail.value).to.equal(30);
+    expect(list.length).to.equal(2);
+  });
+  it("should remove from the head if there is only one item in the list", () => {
+    let list = new List();
+    list.push(10);
+
+    let removedNode = list.remove(0);
+    expect(removedNode.value).to.equal(10);
+    expect(list.head).to.be.null;
+    expect(list.tail).to.be.null;
+    expect(list.length).to.equal(0);
+  });
+});
